5a28944fe5976cfff77844979a06141e44af7cda
IbraMF <76247797+IbraMF@users.noreply.github.com>
2025-09-17 08:42:26 +0200

    minor changes

--- vpn.py
+++ vpn.py
@@ -15,7 +15,7 @@ class Wireguard:
         print("-"*20+"\033[1mWireguard Config\033[0m"+"-"*20)
         print(json.dumps(conf, indent=2))
 
-    def create_instance(self, name: str, private_key: str, public_key: str, listen_port: str, allowed_ips: list[str], peer_id: str, peer_public_key: str, peer_allowed_ips: list[str]) -> None:
+    def create_instance(self, name: str, private_key: str, public_key: str, listen_port: str, allowed_ips: list[str]) -> None:
         print("-"*20+"\033[1mCreating Wireguard Instance\033[0m"+"-"*20)
 
         data = {"data": {
@@ -31,16 +31,20 @@ class Wireguard:
         print(r.text)
         r.raise_for_status()
 
+    def add_peer(self, name: str, peer_id: str, peer_public_key: str, peer_allowed_ips: list[str], route_allowed: str = "1", keepalive: str = "25") -> None:
+        print("-"*20+"\033[1mAdding Peer Wireguard\033[0m"+"-"*20)
         peer_data = {"data":{
             "id": peer_id,
             "public_key": peer_public_key,
             "allowed_ips": peer_allowed_ips,
-            "route_allowed_ips": "1",
-            "persistent_keepalive": "25"
+            "route_allowed_ips": route_allowed,
+            "persistent_keepalive": keepalive
 
         }}
 
         r = requests.post(f"{self.base_url}/{name}/peers/config", headers=self.headers, json=peer_data, verify=False)
+        print(r.text)
+        r.raise_for_status()
 
 class OpenVPN:
     def __init__(self, ip: str, headers: dict):
@@ -70,19 +74,19 @@ class OpenVPN:
         print(r.json())
         return r.json()["data"]["path"]
 
-    def create_instance(self, name: str, port: str, ca_path: str, cert_path: str, key_path: str, remote_host: str, ip: str, mask: str) -> None:
+    def create_instance(self, name: str, protocol:str,  port: str, ca_path: str, cert_path: str, key_path: str, remote_host: str, ip: str, mask: str) -> None:
         print("-"*20+"\033[1mCreating OpenVPN Instance\033[0m"+"-"*20)
 
         new_section = self._get_new_section()
         ca_path = self.upload_certificate(new_section, ca_path, "ca")
-        cert_path = self.upload_certificate(new_section, cert_path, "cert")
         key_path = self.upload_certificate(new_section, key_path, "key")
+        cert_path = self.upload_certificate(new_section, cert_path, "cert")
 
         data = {"data": {
             "enable": "1",
             "name": name,
             "type": "client",
-            "proto": "udp",
+            "proto": protocol,
             "auth_mode": 
             "tls",
             "topology" :"subnet",
